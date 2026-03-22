c456eb53a0304a0727bc57dd9d1f5f415de01a1f
Jan Gebauer <post@jangebauer.eu>
2026-03-22 19:45:37 +0100

    github issues

--- publishconf.py
+++ publishconf.py
@@ -10,15 +10,9 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
 # DISQUS_SITENAME = ""
 # GOOGLE_ANALYTICS = ""
-
-if os.path.exists("output"):
-    shutil.rmtree("output")
